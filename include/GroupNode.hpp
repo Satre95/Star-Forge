c4112f0eac2a585dfb3a31f06f585e9a01a3dd22
Siddharth Atre <sidatre@gmail.com>
2017-10-30 17:58:47 -0700

    Changed erase to use index

--- include/GroupNode.hpp
+++ include/GroupNode.hpp
@@ -4,17 +4,16 @@
 #include "Node.hpp"
 
 namespace starforge {
-	class GroupNode: public Node {
+	class GroupNode : public Node {
 	public:
 		GroupNode(Node * parent = nullptr);
 		virtual ~GroupNode();
-		
+
 		void AddChild(Node * child);
-		void RemoveChild(Node * child);
+		void RemoveChild(size_t i);
 		const std::vector<Node *> & GetChildren() const;
 
 	protected:
 		std::vector<Node *> m_children;
-
 	};
 }
--- source/GroupNode.cpp
+++ source/GroupNode.cpp
@@ -2,24 +2,24 @@
 
 namespace starforge {
 	GroupNode::GroupNode(Node * parent) : Node(parent) {
-
 	}
 
 	GroupNode::~GroupNode() {
-		for(Node * aNode: m_children)
+		for (Node * aNode : m_children)
 			delete aNode;
 		m_children.clear();
 	}
 
-	void GroupNode::AddChild(Node * child) { 
+	void GroupNode::AddChild(Node * child) {
 		m_children.push_back(child);
 	}
-		
-	void GroupNode::RemoveChild(Node * child) { 
-			m_children.erase(std::remove(m_children.begin(), m_children.end(), child), m_children.end());
-		}
-		
-	const std::vector<Node *> & GroupNode::GetChildren() const { 
+
+	void GroupNode::RemoveChild(size_t i) {
+		if (i >= m_children.size()) return;
+		m_children.erase(m_children.begin() + i);
+	}
+
+	const std::vector<Node *> & GroupNode::GetChildren() const {
 		return m_children;
 	}
 }
